b00ac423cc3460ee2cc4cf5524ee126cc4f25136
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-17 14:17:36 +0700

    feat(client): added navbar and login component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,11 @@
-import { useState } from "react";
+import { Login } from "./components/Login";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="flex flex-col gap-4 items-center justify-center h-screen w-screen">
-        <h1 className="text-4xl">{count}</h1>
-        <div className="flex gap-4">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => setCount((prev) => prev + 1)}
-          >
-            +
-          </button>
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => setCount((prev) => prev - 1)}
-          >
-            -
-          </button>
-        </div>
-      </div>
+      <Navbar />
+      <Login />
     </>
   );
 }
---
+++ client/src/components/Login.tsx
@@ -0,0 +1,29 @@
+export const Login = () => {
+  return (
+    <div className="flex flex-col justify-center items-center bg-[#F7EAE4] h-screen">
+      <img src="/logo-1.jpg" alt="logo" className="w-48 h-48" />
+      <form className="flex flex-col gap-4 w-full h-full px-8 max-w-md">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          className="border-2 border-gray-500 rounded-md p-2"
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="border-2 border-gray-500 rounded-md p-2"
+        />
+        <button
+          type="submit"
+          className="bg-[#F7EAE4] border-2 border-gray-500 rounded-md p-2"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
