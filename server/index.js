99258f3bf9f51e9e6c85168421d49f99646b7f48
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-25 13:57:21 +0700

    chore(server): use the new filename for db connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { connectDB } = require("./config/db");
+const { connectDB } = require("./config/mongodb.connection");
 const app = express();
 
 const PORT = process.env.PORT || 8081;
