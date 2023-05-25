a94d4a183ad50dc9473ff9901d06fd72636ce334
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-25 08:26:48 +0700

    refactor(server): refactored `User` model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,19 +1,36 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
+    required: [true, "Username is required"],
     unique: true,
   },
-  name: String,
-  passwordHash: String,
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must be at least 8 characters long"],
+    select: false,
+  },
 });
 
 userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    delete returnedObject.passwordHash;
+    delete returnedObject.password;
   },
+});
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  user.password = await bcrypt.hash(user.password, 10);
+
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
