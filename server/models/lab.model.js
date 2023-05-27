f33a3254e9c9fd7e3139567fde5167a6d87a5a14
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-27 09:39:33 +0700

    feat(server): added `Lab.isReservable` helper method

--- server/models/lab.model.js
+++ server/models/lab.model.js
@@ -20,6 +20,39 @@ const labSchema = new mongoose.Schema({
   },
 });
 
+labSchema.methods.isReservable = async function (start_time, end_time) {
+  const Reservation = require("./reservation.model");
+  const labId = this._id;
+  const reservations = await Reservation.find({
+    $and: [
+      { lab_id: labId },
+      {
+        $or: [
+          {
+            $and: [
+              { start_time: { $lte: start_time } },
+              { end_time: { $gte: start_time } },
+            ],
+          },
+          {
+            $and: [
+              { start_time: { $lte: end_time } },
+              { end_time: { $gte: end_time } },
+            ],
+          },
+          {
+            $and: [
+              { start_time: { $gte: start_time } },
+              { end_time: { $lte: end_time } },
+            ],
+          },
+        ],
+      },
+    ],
+  });
+  return reservations?.length === 0;
+};
+
 const Lab = mongoose.model("Lab", labSchema);
 
 module.exports = Lab;
