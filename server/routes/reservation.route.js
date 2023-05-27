4317174c23e5da6ab9045c96f3edfbd36f794f6a
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-27 09:34:00 +0700

    chore(server): make use of the `isAuthenticated` middleware

--- server/routes/reservation.route.js
+++ server/routes/reservation.route.js
@@ -1,5 +1,7 @@
 const Router = require("express").Router();
 
+const { isAuthenticated } = require("../middlewares/app.authentication");
+
 const {
   getReservations,
   getReservationById,
@@ -7,13 +9,15 @@ const {
   getActiveReservations,
   getPastReservations,
   getFutureReservations,
+  deleteReservation,
 } = require("../controllers/reservation.controller");
 
 Router.get("/reservations", getReservations);
 Router.get("/reservations/:id", getReservationById);
-Router.post("/reservations", createReservation);
+Router.post("/reservations", isAuthenticated, createReservation);
 Router.get("/reservations/active", getActiveReservations);
 Router.get("/reservations/past", getPastReservations);
 Router.get("/reservations/future", getFutureReservations);
+Router.delete("/reservations/:id", isAuthenticated, deleteReservation);
 
 module.exports = Router;
