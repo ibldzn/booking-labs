5b31e2eab1123264b4ee0a964dfc03cff4523e00
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-27 09:39:13 +0700

    refactor(server): make sure we're already connected to the database before doing anything

--- server/index.js
+++ server/index.js
@@ -9,45 +9,46 @@ const app = express();
 
 const PORT = process.env.PORT || 8081;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET_KEY,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24, // 1 day
-    },
-  })
-);
+const start = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET_KEY,
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24, // 1 day
+      },
+    })
+  );
+  app.use(express.json({ extended: false }));
 
-app.use(express.json({ extended: false }));
+  app.use("/api/v1", require("./routes/auth.route"));
+  app.use("/api/v1", require("./routes/labs.route"));
+  app.use("/api/v1", require("./routes/reservation.route"));
 
-connectDB();
+  app.get("/laboran", (req, res) => {
+    res.json([
+      {
+        name: "Teni Amalia",
+        phone: "0821-2151-2892",
+      },
+      {
+        name: "Gina Agnia",
+        phone: "0857-2446-6240",
+      },
+      {
+        name: "Fretty Lendifah E N",
+        phone: "0877-3781-9177",
+      },
+    ]);
+  });
 
-app.use("/api/v1", require("./routes/auth.route"));
-app.use("/api/v1", require("./routes/labs.route"));
-app.use("/api/v1", require("./routes/reservation.route"));
+  app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
+};
 
-app.get("/laboran", (req, res) => {
-  res.json([
-    {
-      name: "Teni Amalia",
-      phone: "0821-2151-2892",
-    },
-    {
-      name: "Gina Agnia",
-      phone: "0857-2446-6240",
-    },
-    {
-      name: "Fretty Lendifah E N",
-      phone: "0877-3781-9177",
-    },
-  ]);
-});
-
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+connectDB().then(start);
