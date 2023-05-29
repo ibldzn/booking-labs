c004fb89569f45ce8d3568feaf898a8a44d21a5f
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-30 05:21:58 +0700

    refactor(client): changed image size

--- client/src/components/ImageCarousel.tsx
+++ client/src/components/ImageCarousel.tsx
@@ -37,7 +37,7 @@ export const ImageCarousel = ({ images }: ImageCarouselProps) => {
       </button>
 
       <img
-        className="w-56 h-36 object-contain"
+        className="w-64 h-44 object-contain"
         src={images[currentImage]}
         alt="image"
       />
