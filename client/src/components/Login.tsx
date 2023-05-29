a2bcc2dcc16396d83d1393527cee0fa9956ada2c
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-29 14:34:04 +0700

    chore(client): update text

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -101,7 +101,7 @@ export const Login = ({
               Sign in
             </Button>
             <Typography color="gray" className="mt-4 text-center font-normal">
-              Already have an account?{" "}
+              Belum punya akun?{" "}
               <Link
                 to="/register"
                 className="font-medium text-blue-500 transition-colors hover:text-blue-700"
