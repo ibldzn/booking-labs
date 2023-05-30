635fd3ea93977ae2adf99e07cb77132b07288328
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-05-31 03:40:56 +0700

    refactor(client): update image height

--- client/src/components/ImageCarousel.tsx
+++ client/src/components/ImageCarousel.tsx
@@ -37,7 +37,7 @@ export const ImageCarousel = ({ images }: ImageCarouselProps) => {
       </button>
 
       <img
-        className="w-64 h-44 object-contain"
+        className="w-64 h-56 object-contain"
         src={images[currentImage]}
         alt="image"
       />
