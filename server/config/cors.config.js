c40f9dff708043fc4641d315e9ba4260ba10ec5e
ibldzn <51160226+ibldzn@users.noreply.github.com>
2023-06-19 20:01:32 +0700

    kasldfjklasdj

--- server/config/cors.config.js
+++ server/config/cors.config.js
@@ -1,12 +1,4 @@
-const allowedOrigins = [
-  "http://localhost:8080",
-  "http://localhost:8081",
-  "http://192.168.1.69:8080",
-  "http://192.168.1.69:8081",
-  "http://192.168.1.10:8080",
-  "http://192.168.1.10:8081",
-  "https://booking-labs.vercel.app/",
-];
+const allowedOrigins = ["https://booking-labs.vercel.app"];
 
 const corsOptions = {
   origin: (origin, next) => {
